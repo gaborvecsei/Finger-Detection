baff00c9cf36000edc25d1c62bc0d4cc079430c4
Gabe <vecseigabor.x@gmail.com>
2016-03-08 00:20:31 +0100

    First Commit

---
+++ Hand_Recognition_homework.m
@@ -0,0 +1,102 @@
+clc;    %Comman képerny? tisztítása
+close all;  %Minden bezárása
+clear;  %Meglév? változók "törlése"
+workspace;  %WorkSpace jön el? a program indítása után
+
+%Kép beolvasása
+[filename, pathname] = ...
+     uigetfile({'*.jpg';'*.png';'*.tif';'*.*'},'Select an Image file');
+fullFileName = fullfile(pathname, filename);
+I = imread(fullFileName);
+
+figure(1)
+subplot(2,2,1)
+imshow(I);
+
+
+%A méretek felvétele
+height = size(I,1);
+width = size(I,2);
+
+%Az O-ra rajzolunk rá pirossal (azaz bejelöljük a b?rfelületet)
+O = I;
+BW = zeros(height,width);
+
+%A képet RGB-b?l YCbCr-ba konvertáljuk
+%https://en.wikipedia.org/wiki/YCbCr
+I_ycbcr = rgb2ycbcr(I);
+Cb = I_ycbcr(:,:,2);
+Cr = I_ycbcr(:,:,3);
+
+figure(1)
+subplot(2,2,2)
+imshow(I_ycbcr);
+
+
+%B?r detektálása
+[r,c,v] = find(Cb>=77 & Cb<=127 & Cr>=133 & Cr<=173);
+numind = size(r,1);
+
+%Mark Skin Pixels
+for i=1:numind
+    %Az eredeti képen beszínezzük a b?rfelületeket (szemléltetés)
+    O(r(i),c(i),:) = [255 0 0];
+    %Az eddig csak 0-ból álló tömbbe a b?r helyét feltöltjük 1-esekkel
+    BW(r(i),c(i)) = 1;
+end
+
+%A kis fekete helyeket a nagyban kitöltjük
+BW = imfill(BW, 'holes');
+%Eltünteti azt ami 300 pixelnél kissebb (zaj sz?rés)
+%Egy már 1024 x 1024-es képen is kicsi az esélye, hogy 300-nál kissebb
+%legyen a kéz területe
+BW = bwareaopen(BW,300);
+
+figure(1)
+subplot(2,2,3)
+imshow(BW);
+
+st = regionprops(BW, 'All');
+
+%Kiírjuk a workspace-n a területeket számszerüsítve
+fprintf(1,'Terulet #    Area\n');
+for k = 1 : length(st)
+    %"Bedobozolás"
+    thisBB = st(k).BoundingBox;
+    hold on
+    rectangle('Position', [thisBB(1),thisBB(2),thisBB(3),thisBB(4)],...
+      'EdgeColor','r','LineWidth',1 );
+    hold off
+    fprintf(1,'#%2d %16.1f\n',k,st(k).Area);
+end
+
+%Rendezzük az elemeket csökken?be a területük szerint
+allAreas = [st.Area];
+[sortedAreas, sortingIndexes] = sort(allAreas, 'descend');
+
+%Megszámozza a legnagyobbtól a legkissebbig
+for k = 1 : length(st)
+    centerX = st(sortingIndexes(k)).Centroid(1);
+    centerY = st(sortingIndexes(k)).Centroid(2);
+    text(centerX,centerY,num2str(k),'Color', 'b', 'FontSize', 14)
+end
+
+%A második legnagyobb terület lesz a kéz (fej az els?)
+% Ez a lépés nem szükséges, ha csak a kezet fotózzuk
+handIndex = sortingIndexes(1);
+%Ha több terület van csak akkor kell tör?dni vele
+if length(sortingIndexes) > 1
+    handIndex = sortingIndexes(2);
+end
+%Eltüntetünk mindent kivéve a kezét
+[labeledImage, numberOfAreas] = bwlabel(BW);
+HandImage = ismember(labeledImage, handIndex);
+
+%Kivágjuk, hogy csak a kéz legyen a képen
+HandImage = imcrop(I, st(handIndex).BoundingBox);
+figure(1)
+subplot(2,2,4)
+imshow(HandImage);
+
+
+
